66e2d36b4be2d017eead6086cf853aa3c625a9b2
boaz1e <boazy@my.hit.ac.il>
2024-02-08 17:02:11 +0200

    Add db.js file

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+module.exports = mongoose;
