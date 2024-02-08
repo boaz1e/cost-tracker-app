16f06d8e7b646b145273466037a695b7bd55979b
boaz1e <boazy@my.hit.ac.il>
2024-02-08 17:03:11 +0200

    Setup Express.js application with middleware and routes

---
+++ src/config/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./db");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(require("../routes/costRoutes"));
+app.use(require("../routes/reportRoutes"));
+app.use(require("../routes/aboutRoutes"));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+module.exports = app;
